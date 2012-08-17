35f827e0805f7698bc9d04eea1c7edddb0984793
macNugs <niftynei@gmail.com>
2012-08-17 18:41:13 -0400

    remove comments and console logs

--- wave.js
+++ wave.js
@@ -7,7 +7,6 @@ function startAnim(e) {
 		e.target.interval = setInterval(function () { 
 				colors.cycle();
 				e.target.style.backgroundColor = "rgb(" + colors.r() +", "+ colors.g() + ", " + colors.b() + ")";
-				//console.log(e.target.style.backgroundColor);
 			}, 100);
 		} else {
 			clearInterval(e.target.interval);
@@ -16,20 +15,12 @@ function startAnim(e) {
 }
 
 function endAnim(e) {
-	// end color swirling (use new prop on elem?)
-	//if (e.target.interval) {
-	//	clearInterval(e.target.interval);
-	//}
 	e.target.style.opacity = 0.75;
 }
 
 function load() {
-	console.log("load elements");
-	// for each div element on the page, attach a listener
 	var elems = document.getElementsByTagName("div");
 	for (var i = 0; i < elems.length; i++) {
-		//elems[elem].addEventListener('mouseenter', startAnim, false);
-		//elems[elem].addEventListener('mouseleave', endAnim, false);
 		elems[i].addEventListener('mouseover', startAnim, false);
 		elems[i].addEventListener('mouseout', endAnim, false);
 	}
